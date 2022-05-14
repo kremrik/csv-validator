28b17041218c6188bd674e86d51a8a388fefc088
kremrik <kyle.robert.e@gmail.com>
2022-05-14 16:03:10 -0500

    more tests

--- src/parser.rs
+++ src/parser.rs
@@ -18,6 +18,7 @@ pub fn get_constraint_map(json_text: &str) -> ConstraintFile {
 #[cfg(test)]
 mod test {
     use super::{
+        Constraint,
         ConstraintFile,
         get_constraint_map,
     };
@@ -25,7 +26,26 @@ mod test {
     #[test]
     fn test_no_constraints() {
         let json_text = "{}";
-        let expect: ConstraintFile = ConstraintFile::new();
+        let expect = ConstraintFile::new();
+        let actual = get_constraint_map(json_text);
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn test_mult_valid_constraints_mult_fields() {
+        let json_text = r#"
+            {
+                "foo": ["NotEmpty", "IsNumber"],
+                "bar": ["NotEmpty", "IsFloat"],
+                "baz": ["NotEmpty"]
+            }
+        "#;
+
+        let mut expect = ConstraintFile::new();
+        expect.insert(String::from("foo"), vec![Constraint::NotEmpty, Constraint::IsNumber]);
+        expect.insert(String::from("bar"), vec![Constraint::NotEmpty, Constraint::IsFloat]);
+        expect.insert(String::from("baz"), vec![Constraint::NotEmpty]);
+
         let actual = get_constraint_map(json_text);
         assert_eq!(expect, actual);
     }
